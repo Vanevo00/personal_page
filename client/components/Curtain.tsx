993d07d301d17f95723243c408f4bbb7df0a589b
Vojtech Vanek <vanevo00@gmail.com>
2020-04-04 17:09:07 +0200

    feat: handle anonymous entry

--- client/components/Curtain.tsx
+++ client/components/Curtain.tsx
@@ -11,37 +11,48 @@ import VisitorContext from '../context/visitor/visitorContext'
 
 const Curtain = () => {
   const [showCurtain, setShowCurtain] = useState(false)
+  const [showLeftText, setShowLeftText] = useState(true)
   const [visitorName, setVisitorName] = useState('')
 
   const nameInput = useRef(null)
   const visitorContext = useContext(VisitorContext)
-
-  const onChange = (e) => {
-    setVisitorName(e.target.value)
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-    localStorage.setItem('visitorName', visitorName)
-    visitorContext.setVisitor(visitorName)
-  }
 
   useEffect(() => {
     if (localStorage.visitorName) {
       visitorContext.setVisitor(localStorage.visitorName)
     }
     setShowCurtain(true)
+
     setTimeout(() => {
       nameInput.current.focus()
     }, 6500)
-
   }, [])
+
+  const onChange = (e) => {
+    setVisitorName(e.target.value)
+  }
+
+  const handleNameSubmission = (name) => {
+    localStorage.setItem('visitorName', name)
+    visitorContext.setVisitor(name)
+    setShowLeftText(false)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    handleNameSubmission(visitorName)
+  }
+
+  const onDeclineName = (e) => {
+    e.preventDefault()
+    handleNameSubmission('Anonymous')
+  }
 
   return (
     <CurtainContainer>
       <CurtainLeft show={showCurtain}>
-        <CurtainParagraph show={showCurtain} delay={2.5}>Hi, my name is Vojta.</CurtainParagraph>
-        <CurtainParagraph show={showCurtain} delay={4}>I am a JavaScript developer.</CurtainParagraph>
+        <CurtainParagraph show={showCurtain && showLeftText} delay={showLeftText ? 2.5 : 0}>Hi, my name is Vojta.</CurtainParagraph>
+        <CurtainParagraph show={showCurtain && showLeftText} delay={showLeftText ? 4 : 0}>I am a JavaScript developer.</CurtainParagraph>
       </CurtainLeft>
 
       <MiddleLogo show={showCurtain}>
@@ -58,7 +69,7 @@ const Curtain = () => {
           <form onSubmit={onSubmit}>
             <NameInput type='text' ref={nameInput} show={showCurtain} delay={5.5} onChange={onChange} value={visitorName}/>
             <CurtainButton type='submit' show={showCurtain} delay={5.5}>Submit</CurtainButton>
-            <CurtainButton show={showCurtain} delay={7.5}>I'm not giving my name to a machine!</CurtainButton>
+            <CurtainButton show={showCurtain} delay={7.5} onClick={onDeclineName}>I'm not giving my name to a machine!</CurtainButton>
           </form>
       </CurtainRight>
     </CurtainContainer>
